2d4b06290b30b2049b71e91d72a4ad91e807c3d8
Jonathan Dai <nightghost5078@gmail.com>
2013-08-20 15:33:55 +0800

    Init ver.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "lib/log/Log.h"
+
+using namespace XCF;
+
+int main() {
+    Log *logger = LogFactory::get(LogType::SysLog);
+
+    logger->setPriority(LogPriority::Info);
+
+    logger->debug("debug message!");
+    logger->info("info message!");
+    logger->warn("warning message!");
+    logger->error("error message!");
+
+    logger->output();
+
+    return 0;
+}
